37fd7436004247fe994390bf9259e5ee5085af8a
Joel Semar <semarjt@gmail.com>
2013-12-05 21:39:18 -0600

    ..

--- urls.py
+++ urls.py
@@ -12,11 +12,11 @@ import consts
 urlpatterns = patterns('',
     (r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
     (r'^$', object_list, {'template_name': 'index.html', 'queryset': Group.objects.all(),
-        'template_object_name': 'groups', 'extra_context': {'states' :consts.SUPPORTED_STATES}}),
+        'template_object_name': 'group_list', 'extra_context': {'states' :consts.SUPPORTED_STATES}}),
     (r'^submit/?$', 'main.views.submit'),
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/?', include(admin.site.urls)),
     (r'', object_list, {'template_name': 'index.html', 'queryset': Group.objects.all(),
-        'template_object_name': 'groups', 'extra_context': {'states' :consts.SUPPORTED_STATES}}),
+        'template_object_name': 'group_list', 'extra_context': {'states' :consts.SUPPORTED_STATES}}),
  
 )
 
