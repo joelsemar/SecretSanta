1e69e40eb20ce67357e243f86a1d7dcb3e20185d
Joel Semar <semarjt@gmail.com>
2013-12-05 21:29:05 -0600

    groups

--- main/models.py
+++ main/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.core.mail import send_mail
 # Create your models here.
 #
+
+class Group(models.Model):
+    name = models.CharField(max_length=256)
+    def __unicode__(self):
+        return self.name
+
 class Entrant(models.Model):
     name = models.CharField(max_length=256)
+    group = models.ForeignKey(Group)
     email = models.CharField(max_length=256)
     zip = models.CharField(max_length=256, default='', blank=True)
     city = models.CharField(max_length=256, default='', blank=True)
